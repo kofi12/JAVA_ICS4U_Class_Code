df420946c4abe80a8ba0f22de3995d33c1254237
Aaron Kofi Haizel <kofi12@my.yorku.ca>
2024-06-27 15:19:46 -0400

    changes

--- class_code/StringFormatting.java
+++
@@ -1,9 +0,0 @@
-package class_code;
-
-public class StringFormatting {
-    public static void main(String[] args) {
-
-        double money = 150.55;
-        System.out.printf("You have $%.1f dollars in your bank account", money);
-    }
-}
