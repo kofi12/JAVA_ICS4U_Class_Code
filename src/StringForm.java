101ff198eff43ed1ee3181f899d2d4065a2e7362
Aaron Kofi Haizel <kofi12@my.yorku.ca>
2024-10-09 11:44:22 -0400

    Added files for string format ect.

---
+++ src/StringForm.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class StringForm {
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+       System.out.print("Enter a number: ");
+       int number = input.nextInt();
+       boolean isEven = (number % 2 == 0);
+       boolean isSingleDigit = number < 10;
+
+       if(isEven && isSingleDigit) {
+           System.out.println(number + " is even and is a single digit");
+       }
+       else if(isEven && !isSingleDigit) {
+           System.out.println(number + " is even and not a single digit");
+       }
+       else if(!isEven && isSingleDigit) {
+           System.out.println(number + " is a single digit and odd");
+       }
+       else{
+           System.out.println(number + " is not a single digit and odd");
+       }
+    }
+}
